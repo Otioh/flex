50a8078da41400266fce044bc58b27efb14442b4
Emmanuel Erim <bryonerim@gmail.com>
2023-03-24 15:07:56 +0100

    flex nav done

--- App.js
+++ App.js
@@ -9,16 +9,7 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{title: 'Welcome'}}
-        />
-        <Stack.Screen name="Start" component={Start} />
-      </Stack.Navigator>
-    </NavigationContainer>
+   <Start/>
 
   );
 }
